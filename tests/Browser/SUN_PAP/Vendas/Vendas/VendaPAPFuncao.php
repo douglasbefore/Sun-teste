64c37155065c5e3f06979054d43cd91fc2cdff6a
douglasbefore <douglas.colussi@before.com.br>
2018-05-26 11:49:21 -0400

    Inicio da venda no PAP para o Vendedor.

--- tests/Browser/SUN_PAP/Vendas/Vendas/VendaPAPFuncao.php
+++ tests/Browser/SUN_PAP/Vendas/Vendas/VendaPAPFuncao.php
@@ -13,34 +13,34 @@ use Laravel\Dusk\Browser;
 class VendaPAPFuncao
 {
     public function PreencherCamposDadosCliente(Browser $browser){
-        if($browser->value('@CampoClienteNomeCompleto') == "") {
-            $browser->type('@CampoClienteNomeCompleto', 'Teste Teste Teste');
-            $browser->type('@CampoClienteDataNascimento', '11111946');
-            $browser->type('@CampoClienteNomeMae', 'Mae Teste Teste');
-            $browser->click('@BotaoClienteSexoMasculino');
-            $browser->type('@CampoClienteEmail', 'testeteste@teste.com.br');
-            $browser->type('@CampoClienteTelefoneCelular', '67985856498');
+        if($browser->value(CampoVenda::CampoClienteNomeCompleto) == "") {
+            $browser->type(CampoVenda::CampoClienteNomeCompleto, 'Teste Teste Teste');
+            $browser->type(CampoVenda::CampoClienteDataNascimento, '11111946');
+            $browser->type(CampoVenda::CampoClienteNomeMae, 'Mae Teste Teste');
+            $browser->click(CampoVenda::BotaoClienteSexoMasculino);
+            $browser->type(CampoVenda::CampoClienteEmail, 'testeteste@teste.com.br');
+            $browser->type(CampoVenda::CampoClienteTelefoneCelular, '67985856498');
         }else{
-            if($browser->value('@CampoClienteNomeCompleto') == ""){
-                $browser->type('@CampoClienteNomeCompleto', 'Teste Teste Teste');
+            if($browser->value(CampoVenda::CampoClienteNomeCompleto) == ""){
+                $browser->type(CampoVenda::CampoClienteNomeCompleto, 'Teste Teste Teste');
             }
-            if($browser->value('@CampoClienteDataNascimento') == "") {
-                $browser->type('@CampoClienteDataNascimento', '11111946');
+            if($browser->value(CampoVenda::CampoClienteDataNascimento) == "") {
+                $browser->type(CampoVenda::CampoClienteDataNascimento, '11111946');
             }
-            if($browser->value('@CampoClienteNomeMae') == "") {
-                $browser->type('@CampoClienteNomeMae', 'Mae Teste Teste');
+            if($browser->value(CampoVenda::CampoClienteNomeMae) == "") {
+                $browser->type(CampoVenda::CampoClienteNomeMae, 'Mae Teste Teste');
             }
-            if($browser->value('@BotaoSexoMasculino') == "") {
-                $browser->click('@BotaoSexoMasculino');
+            if($browser->value(CampoVenda::BotaoClienteSexoMasculino) == "") {
+                $browser->click(CampoVenda::BotaoClienteSexoMasculino);
             }
-            if($browser->value('@CampoClienteEmail') == "") {
-                $browser->type('@CampoClienteEmail', 'testeteste@teste.com.br');
+            if($browser->value(CampoVenda::CampoClienteEmail) == "") {
+                $browser->type(CampoVenda::CampoClienteEmail, 'testeteste@teste.com.br');
             }
-            if($browser->value('@CampoClienteTelefoneCelular') == "") {
-                $browser->type('@CampoClienteTelefoneCelular', '67985856498');
+            if($browser->value(CampoVenda::CampoClienteTelefoneCelular) == "") {
+                $browser->type(CampoVenda::CampoClienteTelefoneCelular, '67985856498');
             }
-            if($browser->value('@CampoClienteTelefoneCelular') == "" || strlen($browser->value('@CampoClienteTelefoneCelular')) != 10) {
-                $browser->type('@CampoClienteTelefoneFixo', '6733331111');
+            if($browser->value(CampoVenda::CampoClienteTelefoneCelular) == "" || strlen($browser->value(CampoVenda::CampoClienteTelefoneCelular)) != 10) {
+                $browser->type(CampoVenda::CampoClienteTelefoneFixo, '6733331111');
             }
         }
     }
